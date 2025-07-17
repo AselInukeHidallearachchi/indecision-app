d72247902cfdc6b1216291a91012dbc04497b6fa
Asel Hidallearachchi <aselinuke@gmail.com>
2025-07-17 23:25:14 +0530

    finished up to Hello React

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,13 @@
-import React from "react";
-
 console.log("Hello, World!");
 
 //JSX  - javascript XML
-var template = <p>This is JSX for app.js </p>;
+//var template = <p>This is JSX for app.js </p>;
+
+var template = React.createElement(
+  "h1",
+  { id: "title" },
+  "This is JSX for app.js"
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
