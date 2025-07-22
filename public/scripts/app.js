e4d21619f429c628d40d8743705755d2435129e3
Asel Hidallearachchi <aselinuke@gmail.com>
2025-07-22 10:02:19 +0530

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,59 @@
 
 console.log("Hello, World!");
 
+var firstname = "Asel";
+
 //JSX  - javascript XML
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision App "
+  React.createElement(
+    "h1",
+    null,
+    firstname
+  ),
+  " ",
+  React.createElement(
+    "p",
+    null,
+    "Put your life in the hands of a computer"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Option 1"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Option 2"
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Andreq Mead"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 26"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: New York"
+  )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 console.log("Hello, World!");
 
+var firstname = "Asel";
+
 //JSX  - javascript XML
-var template = <h1>Indecision App </h1>;
+var template = (
+  <div>
+    <h1>{firstname}</h1> <p>Put your life in the hands of a computer</p>
+    <ol>
+      <li>Option 1</li>
+      <li>Option 2</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Andreq Mead</h1>
+    <p>Age: 26</p>
+    <p>Location: New York</p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
